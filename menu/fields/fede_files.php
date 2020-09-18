c5061e896c980196638bf85f6fcfbbce00b923fc
fetitxe <fetitxe@users.noreply.github.com>
2020-09-18 12:32:17 +0200

    to 1.5.10

--- menu/fields/fede_files.php
+++ menu/fields/fede_files.php
@@ -19,14 +19,14 @@ function fed_form_files($options){
 	$required = fed_get_data( 'is_required', $options ) == 'true' ? 'required="required"' : null;
 	$id       = fed_get_data( 'id_name', $options ) != '' ? 'id="' . esc_attr( $options['id_name'] ) . '"' : null;
 	$img      = '';
-	if ( ! empty( $value ) ) {
+	if( !empty($value) ){
 		$options['user_value'] = (int) $value;
-		$img                   = fede_get_image_by_type( $options );
+		$img                   = fede_get_image_by_type($options);
 		$is_image              = true;
-		if ( empty( $img ) ) {
+		if( empty($img) ){
 			$is_image = false;
 		}
-	} else {
+	}else{
 		$options['user_value'] = '';
 		$is_image              = false;
 	}
@@ -49,10 +49,10 @@ function fed_form_files($options){
 				</div>',
 		$class,
 		$id,
-		$is_image ? 'fed_hide' : '',
+		$is_image? 'hide' : '',
 		$fa_icon,
-		! $is_image ? 'fed_hide' : '',
-		$img,
+		!$is_image? 'hide' : '',
+		htmlentities($img),
 		$name,
 		$options['user_value'],
 		$is_image ? '<span class="fed_remove_image">X</span>' : '<span class="fed_remove_image fed_hide">X</span>'
